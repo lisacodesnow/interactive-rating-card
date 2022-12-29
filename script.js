87d564f4aef795d3eecb314c850e3128e3e0f21b
Lisa T <llmt2007@aol.com>
2022-12-29 10:08:31 -0500

    Final version without notes

--- script.js
+++ script.js
@@ -1,38 +1,27 @@
 // JavaScript Document
 
-const button = document.querySelectorAll(".button_1"); //because its a list with the same class I have to use querySelectorAll so it can select the list. Because its a list I don't have to put the buttons in an array
+const button = document.querySelectorAll(".button_1"); 
 const frontPage = document.querySelector(".front_content-wrapper");
 const thankYouPage = document.querySelector(".thank_you_content-wrapper")
 const submitButton = document.querySelector(".submit_button");
 const ratingSelected = document.querySelector("span");
-
 const goBack = document.querySelector(".go_back");
 
 
-/*submitButton.addEventListener("click",function(){
+button.forEach(function(buttonPushed){
+	buttonPushed.addEventListener("click", function(){
+	ratingSelected.innerHTML = buttonPushed.innerHTML;
+	submitButton.addEventListener("click", function(){
 	frontPage.classList.add("hide");
 	thankYouPage.classList.remove("hide");
-}); */
-
-//forEach function is used to loop through an array, list, which is what button is by using the querySelectorAll
-	button.forEach(function(buttonPushed){
-		buttonPushed.addEventListener("click", function(){
-			ratingSelected.innerHTML = buttonPushed.innerHTML;
-			submitButton.addEventListener("click", function(){
-				frontPage.classList.add("hide");
-				thankYouPage.classList.remove("hide");
 			})
-				});
+	});
 	
-});
+});	
 
 goBack.addEventListener("click", function(){
 	frontPage.classList.remove("hide");
 	thankYouPage.classList.add("hide");
 	submitButton.value = "";
-	});
+});
 
-/*goBack.addEventListener("click", function(){
-	frontPage.classList.remove("hide");
-	thankYouPage.classList.add("hide");
-});*/
